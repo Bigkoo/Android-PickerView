bedca83316f69928e648400e2e1741f4ed3d1c2f
xuhuan <xu.huan.web.master@gmail.com>
2015-10-09 19:25:25 +0800

    增加扩展按钮功能，窗体顶部增加灰色横线

--- README.md
+++ README.md
@@ -2,12 +2,12 @@ PickerView
 ==========
 
 仿iOS的PickerView控件，有时间选择和选项选择并支持一二三级联动效果   
-——TimePopupWindow  时间选择器，支持年月日时分，年月日，年月，时分等格式   
+——TimePopupWindow  时间选择器，支持年月日时分，年月日，年月，时分等格式，支持N个扩展按钮，建议用于 '至今' 功能即可
 ——OptionsPopupWindow  选项选择器，支持一，二，三级选项选择，并且可以设置是否联动    
 
 ####使用gradle 依赖:
 ```java
-   compile 'com.bigkoo:pickerview:1.0.2'
+   compile 'com.bigkoo:pickerview:1.0.3'
 ```
 
 ## Demo 图片（招行信用卡的“掌上生活”里面条件选择器他们用的就是我这个库，大家可以当实际项目参考）
--- app/src/main/java/com/bigkoo/pickerviewdemo/MainActivity.java
+++ app/src/main/java/com/bigkoo/pickerviewdemo/MainActivity.java
@@ -1,11 +1,8 @@
 package com.bigkoo.pickerviewdemo;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +12,10 @@ import com.bigkoo.pickerview.OptionsPopupWindow;
 import com.bigkoo.pickerview.TimePopupWindow;
 import com.bigkoo.pickerview.TimePopupWindow.OnTimeSelectListener;
 import com.bigkoo.pickerview.TimePopupWindow.Type;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 
 public class MainActivity extends Activity {
@@ -30,8 +31,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tvTime=(TextView) findViewById(R.id.tvTime);
-        tvOptions=(TextView) findViewById(R.id.tvOptions);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        tvOptions = (TextView) findViewById(R.id.tvOptions);
         //时间选择器
         pwTime = new TimePopupWindow(this, Type.YEAR_MONTH);
         pwTime.setTime(new Date());
@@ -42,13 +43,24 @@ public class MainActivity extends Activity {
             public void onTimeSelect(Date date) {
                 tvTime.setText(getTime(date));
             }
+
+            @Override
+            public void onButtonClick(int index) {
+                Log.v("onButtonClick", index + "");
+            }
         });
+        //扩展按钮，可以多个，但是考虑宽度问题，建议少一点，
+        //需要的时候增加下面代码即会出现按钮，去掉则无。
+        //上面的 onButtonClick 传回添加按钮时的唯一值，可以根据这个值来处理业务逻辑。
+        //注意：扩展按钮点击的时候不会触发 onTimeSelect 事件！！！
+        pwTime.addButton(this, "扩展按钮1", 1);
+        pwTime.addButton(this, "扩展按钮2", 2);
         //弹出时间选择器
         tvTime.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                pwTime.showAtLocation(tvTime, Gravity.BOTTOM, 0, 0,new Date());
+                pwTime.showAtLocation(tvTime, Gravity.BOTTOM, 0, 0, new Date());
             }
         });
 
@@ -60,11 +72,11 @@ public class MainActivity extends Activity {
         options1Items.add("湖南");
 
         //选项2
-        ArrayList<String> options2Items_01=new ArrayList<String>();
+        ArrayList<String> options2Items_01 = new ArrayList<String>();
         options2Items_01.add("广州");
         options2Items_01.add("佛山");
         options2Items_01.add("东莞");
-        ArrayList<String> options2Items_02=new ArrayList<String>();
+        ArrayList<String> options2Items_02 = new ArrayList<String>();
         options2Items_02.add("长沙");
         options2Items_02.add("岳阳");
         options2Items.add(options2Items_01);
@@ -73,24 +85,24 @@ public class MainActivity extends Activity {
         //选项3
         ArrayList<ArrayList<String>> options3Items_01 = new ArrayList<ArrayList<String>>();
         ArrayList<ArrayList<String>> options3Items_02 = new ArrayList<ArrayList<String>>();
-        ArrayList<String> options3Items_01_01=new ArrayList<String>();
+        ArrayList<String> options3Items_01_01 = new ArrayList<String>();
         options3Items_01_01.add("白云");
         options3Items_01_01.add("天河");
         options3Items_01_01.add("海珠");
         options3Items_01_01.add("越秀");
         options3Items_01.add(options3Items_01_01);
-        ArrayList<String> options3Items_01_02=new ArrayList<String>();
+        ArrayList<String> options3Items_01_02 = new ArrayList<String>();
         options3Items_01_02.add("南海");
         options3Items_01.add(options3Items_01_02);
-        ArrayList<String> options3Items_01_03=new ArrayList<String>();
+        ArrayList<String> options3Items_01_03 = new ArrayList<String>();
         options3Items_01_03.add("常平");
         options3Items_01_03.add("虎门");
         options3Items_01.add(options3Items_01_03);
 
-        ArrayList<String> options3Items_02_01=new ArrayList<String>();
+        ArrayList<String> options3Items_02_01 = new ArrayList<String>();
         options3Items_02_01.add("长沙1");
         options3Items_02.add(options3Items_02_01);
-        ArrayList<String> options3Items_02_02=new ArrayList<String>();
+        ArrayList<String> options3Items_02_02 = new ArrayList<String>();
         options3Items_02_02.add("岳1");
         options3Items_02.add(options3Items_02_02);
 
@@ -110,8 +122,8 @@ public class MainActivity extends Activity {
             public void onOptionsSelect(int options1, int option2, int options3) {
                 //返回的分别是三个级别的选中位置
                 String tx = options1Items.get(options1)
-                        +options2Items.get(options1).get(option2)
-                        +options3Items.get(options1).get(option2).get(options3);
+                        + options2Items.get(options1).get(option2)
+                        + options3Items.get(options1).get(option2).get(options3);
                 tvOptions.setText(tx);
             }
         });
